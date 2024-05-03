da5dcb95b2583c1bf6baa7907d0f6750ec5378c1
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-05-03 05:43:18 +0200

    backend.py sans modifier le repo distant

--- backend.py
+++ backend.py
@@ -41,6 +41,7 @@ def root():
 
 # Exécuter l'application FastAPI sur le port 8000
 
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)"""
+
